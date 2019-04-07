c940f2ed5109a135722b974b5fc238d36a184ede
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-07 11:32:00 +0200

    ask for password

--- pkg/flags/parse.go
+++ pkg/flags/parse.go
@@ -3,14 +3,30 @@ package flags
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type Flags struct {
 	user      string
 	dbname    string
+	dbpass    string
 	sslmode   bool
 	WhiteList []string
+}
+
+func Askpass(prompt string) (string, error) {
+	fmt.Fprintf(os.Stderr, "%s: ", prompt)
+	pw1, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Fprintf(os.Stderr, "\n")
+	if err != nil {
+		return "", err
+	}
+	return string(pw1), nil
 }
 
 // TODO(akarki15): Implement for multiple dbs
@@ -23,12 +39,17 @@ func (f Flags) ConnString() string {
 	if f.sslmode {
 		sslmode = "require"
 	}
+
 	flagStr := fmt.Sprintf("user=%s dbname=%s sslmode=%s", f.user, f.dbname, sslmode)
+	if f.dbpass != "" {
+		flagStr += fmt.Sprintf(" password='%s'", f.dbpass)
+	}
 	return flagStr
 }
 
 func Parse() Flags {
 	user := flag.String("user", "", "username of postgres db")
+	askForPass := flag.Bool("W", false, "ask for password")
 	dbname := flag.String("dbname", "", "dbname for which you want to generate dot file")
 	sslmode := flag.Bool("sslmode", false, "enable sslmode for postgres db connection")
 	whitelist := flag.String("whitelist", "", "comma separated list of tables you want to generate dot file for")
@@ -42,5 +63,13 @@ func Parse() Flags {
 	if len(*whitelist) > 0 {
 		tempFlags.WhiteList = strings.Split(*whitelist, ",")
 	}
+	if *askForPass {
+		p, err := Askpass(fmt.Sprintf("password for %s@%s", *user, *dbname))
+		if err != nil {
+			log.Fatalf("error reading password: %s", err.Error())
+		}
+		tempFlags.dbpass = p
+	}
+
 	return tempFlags
 }
